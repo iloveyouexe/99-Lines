700790ad9708e3e83a323a897379b454c440e7f5
Hunter Hamilton <iloveyouexe1@gmail.com>
2022-02-15 13:30:54 -0600

    final commit

--- app.js
+++ app.js
@@ -1,20 +1,43 @@
 let friends = ["Zuka", "Kaley", "Jon", "Daze", "Zeal"];
-let word = "Lines ";
+const btn = document.getElementById("btn");
 
-for (let i = 0; i < 4; i++) {
-  let count = 99;
-  while (count > 1) {
-    console.log(count + " " + word + "of code in the file,");
-    console.log(count + " " + word + "of code,");
-    console.log(friends[i] + " " + "strikes one out, clears it all out,");
-    count = count - 1;
+btn.addEventListener("click", function () {
+  for (let i = 0; i < friends.length; i++) {
+    const div = document.createElement("div");
+    div.className = "friend";
+
+    const h3 = document.createElement("h3");
+    h3.textContent = friends[i];
+
+    div.appendChild(h3);
+    document.body.appendChild(div);
+
+    for (let x = 99; x >= 1; x--) {
+      const p = document.createElement("p");
+
+      if (x === 2) {
+        p.textContent =
+          "2 lines of code in the file, 2 lines of code; " +
+          friends[i] +
+          " strikes one out, clears it all out, 1 line of code in the file";
+      } else if (x === 1) {
+        p.textContent =
+          "1 line of code in the file, 1 line of code; " +
+          friends[i] +
+          " strikes one out, clears it all out, no more lines of code in the file";
+      } else {
+        p.textContent =
+          x +
+          " lines of code in the file, " +
+          x +
+          " lines of code; " +
+          friends[i] +
+          " strikes one out, clears it all out, " +
+          (x - 1) +
+          " lines of code in the file";
+      }
+
+      div.appendChild(p);
+    }
   }
-
-  if (count == 1) {
-    let word = "Line ";
-    console.log(count + " " + word + "of code in the file.");
-  }
-
-  console.log(friends[i] + " strikes one out, clears it all out,");
-  console.log("No more lines of code in the file");
-}
+});
